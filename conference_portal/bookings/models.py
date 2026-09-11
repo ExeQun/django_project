9485bce6a624aa3834fd03eb080c3e6191f67ada
vladislavboev2007 <vladislav.boev02@vladislavboev2007>
2026-09-11 13:45:20 +0300

    1

---
+++ conference_portal/bookings/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.utils import timezone
+from users.models import CustomUser
+
+class Booking(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'Новая'),
+        ('scheduled', 'Мероприятие назначено'),
+        ('completed', 'Завершено'),
+    ]
+
+    PAYMENT_CHOICES = [
+        ('offline', 'Очное посещение'),
+        ('sbp', 'Перевод по системе СБП')
+    ]
+
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='bookings',
+        verbose_name='Пользователь'
+    )
+    room_name = models.CharField(
+        max_length=255,
+        verbose_name='Название помещения'
+    )
+    conference_date = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name='Дата и время начала конференции'
+    )
+    payment_method = models.CharField(
+        max_length=20,
+        choices=PAYMENT_CHOICES,
+        verbose_name='Способ оплаты'
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='new',
+        verbose_name='Статус'
+    )
+    review = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Отзыв'
+    )
+    created_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата создания' 
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата обновления'
+    )
+
+    def __str__(self):
+        return f'{self.user.username} - {self.room_name} - {self.conference_date}'
+
+    class Meta:
+        verbose_name = 'Бронирование',
+        verbose_name_plural = 'Бронирования'
+        ordering = ['-created_at']
+
